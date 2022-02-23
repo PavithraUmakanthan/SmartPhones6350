1d3bc0c8b762713b01e53f1a6076c6b45bf490c9
Pavi <u.pavithraa@gmail.com>
2022-02-23 13:26:49 -0800

    Assignment4

---
+++ Assignment4/Assignment4/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Assignment4
+//
+//  Created by Venkatanathan Annamalai on 2/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, SendFirstAndLastNameDelegate {
+  
+    @IBOutlet weak var lblFirstName: UILabel!
+    @IBOutlet weak var lblLastName: UILabel!
+    
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getNameFromUser(_ sender: Any) {
+        performSegue(withIdentifier: "segueSecondVC", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueSecondVC"{
+            let getNameVC = segue.destination as! GetNameViewController
+            guard let firstName = lblFirstName.text else{return}
+            guard let lastName = lblLastName.text else{return}
+            getNameVC.firstName = firstName
+            getNameVC.lastName = lastName
+            
+            getNameVC.sendFirstAndLastNameDelegate = self
+    }
+}
+    func setFirstAndLastName(firstName: String, lastName: String) {
+        lblFirstName.text = firstName
+        lblLastName.text = lastName
+    }
+    
+    func setWelcomeText(welcomeText: String) {
+        lblWelcome.text = welcomeText
+    }
+}
+
